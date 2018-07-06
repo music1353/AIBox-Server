2adf686fcda3478fa0ee5a4ef1e33ca4eff82183
蘇靖軒 <music1353@gmail.com>
2018-07-06 15:47:56 +0800

    刪除test

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-flag = ''
-sentence = '請問台北市中山路的餐廳'
-response_info = {'flag': flag, 'response': sentence}
-message = requests.post("http://127.0.0.1:5000/api/chatbot", json=response_info)
-print(message.text)
-dic = json.loads(message.text)
