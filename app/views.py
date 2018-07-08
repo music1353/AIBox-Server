3f2d8158855f8393ac92b8bcaf3ab12927655980
蘇靖軒 <music1353@gmail.com>
2018-07-08 23:03:41 +0800

    新增street domain & 更改成mydict繁體 & login collection

--- app/views.py
+++ app/views.py
@@ -6,6 +6,8 @@ from app.modules.domain_chatbot.chatbot import Chatbot
 from threading import Timer
 import os
 import pymongo
+import time
+import threading
 
 matcher = Matcher()
 matcher.load_rule_data(os.path.join(BASE_DIR, 'domain_matcher/rule'))
@@ -16,6 +18,25 @@ print(matcher.rule_data)
 client = pymongo.MongoClient(MONGO_URI)
 db = client['aiboxdb']
 print('success connect to mongodb.')
+
+# 登出倒數計時方法
+times = 30 # global倒數var
+def logout_timeout():
+    global times
+    print('start logout timeout thread !')
+    print('登出倒數', times, '秒')
+    while times != 0:
+        time.sleep(1)
+        times = times - 1
+        if times == 0:
+            try:
+                user_collect = db['users']
+                user_collect.update_many({}, {'$set':{'is_login': False}})
+                print('登出成功!')
+            except:
+                print('登出錯誤!')
+                
+                
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -42,15 +63,15 @@ def login():
         
         # 紀錄登入狀況
         login_collect = db['login']
-        login_collect.update({'_id': 0},{'$set':{'is_login': True, "user_nickname": user_nickname}})
+        login_collect.update({'_id': 0},{'$set':{"is_login": True, "user_nickname": user_nickname}})
         return jsonify(resp)
 
     
 @app.route('/api/logout', methods=['POST'])
 def logout():
 
-    user_collect = db['users']
-    user_collect.update_many({}, {'$set':{'is_login': False}})
+    login_collect = db['login']
+    login_collect.update({'_id': 0}, {'$set':{'is_login': False, 'user_nickname': ''}})
     
     resp = {
         'status': 200,
@@ -85,11 +106,16 @@ def chatbot_resp():
 
     # 去db找，是否有此專屬語
     # 有則登入，無則聽沒
+    login_collect = db['login']
     user_collect = db['users']
-    user = user_collect.find_one({'nickname': sentence})
-    user_nickname = ''
-    if user is None:
+    has_user_nickname_doc = user_collect.find_one({'nickname': sentence})
+    
+    login_doc = login_collect.find_one({'_id': 0})
+    user_nickname = login_doc['user_nickname'] # 登入中的user的nickname
+    
+    if has_user_nickname_doc is None:
         print('沒有此nickname逆')
+        print('user_nickname:', user_nickname)
 
         # 分析語意
         domain_score = matcher.match_domain(sentence, user_nickname=user_nickname, flag=flag)
@@ -99,13 +125,18 @@ def chatbot_resp():
         message = chat.response_word()
         return message
     else:
-        user_collect.update({'nickname': user_nickname},{'$set':{'is_login': True}})
+        login_collect.update({'_id': 0}, {'$set': {'is_login': True, "user_nickname": has_user_nickname_doc['nickname']}})
         print(user_nickname)
-
         resp = {
             'flag': '',
             'response': '登入成功'
         }
+        
+        # 登出倒數
+        # 是否為登入狀態，並計時倒數
+#        t = threading.Thread(target=logout_timeout, name='logoutTimeoutThread')
+#        t.start()
+        
         return jsonify(resp)
 
     
